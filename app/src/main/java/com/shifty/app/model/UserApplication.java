41d11b4c196e6dae7791b884134ba0699e029d24
Sergio Salazar <sa.sb@outlook.com>
2023-03-07 23:05:06 -0800

    UserApplication API fixes

--- app/src/main/java/com/shifty/app/model/UserApplication.java
+++ app/src/main/java/com/shifty/app/model/UserApplication.java
@@ -17,49 +17,49 @@ import lombok.Data;
 @Entity
 @Table(name = "user_application")
 @Data
-@NoArgsConstructor
-//@IdClass(UserApplicationId.class)
+// @NoArgsConstructor
+// @IdClass(UserApplicationId.class)
 // @AllArgsConstructor
 public class UserApplication {
 
-	@EmbeddedId
-	private UserApplicationId userApplicationId;
+    @EmbeddedId
+    private UserApplicationId userApplicationId;
 
-	@ManyToOne(fetch = FetchType.EAGER, optional = false)
-	@MapsId("user_id")
-	@JoinColumn(name = "user_id", nullable = false)
-	@JsonIgnore 
-	private User user;
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @MapsId("user_id")
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private User user;
 
-	@ManyToOne(fetch = FetchType.EAGER, optional = false)
-	@MapsId("application_id")
-	@JoinColumn(name = "application_id", nullable = false)
-	@JsonIgnore 
-	private Application application;
-	
-	
-    public UserApplication() {}
-    
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @MapsId("application_id")
+    @JoinColumn(name = "application_id", nullable = false)
+    @JsonIgnore
+    private Application application;
+
+    public UserApplication() {
+    }
+
     public UserApplication(User user, Application application) {
         this.user = user;
         this.application = application;
         setUserApplicationId();
     }
+
     private void setUserApplicationId() {
-    	this.userApplicationId= new UserApplicationId(this.user.getUserId(), this.application.getApplicationId());
-    }
- 
-    public void setUser(User user) {
-    	this.user = user;
-    }
-    
-    public void setApplication(Application application){
-    	this.application = application;
+        this.userApplicationId = new UserApplicationId(this.user.getUserId(), this.application.getApplicationId());
     }
 
-	public User getUser() {
-		return user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+
+    public User getUser() {
+        return user;
+    }
 
 }
-
--- app/src/main/java/com/shifty/app/model/UserApplicationId.java
+++ app/src/main/java/com/shifty/app/model/UserApplicationId.java
@@ -10,17 +10,18 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 @Embeddable
 @Data
-@NoArgsConstructor
+// @NoArgsConstructor
 public class UserApplicationId implements Serializable {
-    
-	private Long user_id;
-	private Long application_id;
+
+    private Long user_id;
+    private Long application_id;
 
     public UserApplicationId(Long userId, Long applicationId) {
-    	this.user_id= userId;
+        this.user_id = userId;
         this.application_id = applicationId;
     }
 
-    public UserApplicationId() {}
+    public UserApplicationId() {
+    }
 
 }
