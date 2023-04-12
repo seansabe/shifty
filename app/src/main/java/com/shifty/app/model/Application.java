79f52e6373f659068cf3043defbc90af2a192395
Sergio Salazar <sa.sb@outlook.com>
2023-04-11 17:04:17 -0700

    Removed unused lines

--- app/src/main/java/com/shifty/app/model/Application.java
+++ app/src/main/java/com/shifty/app/model/Application.java
@@ -2,7 +2,6 @@ package com.shifty.app.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
